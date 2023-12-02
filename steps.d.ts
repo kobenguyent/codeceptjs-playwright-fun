c129d8c89cdc10c9ced397b4e6a0c3cb34cd384b
KobeNguyenT <7845001+kobenguyent@users.noreply.github.com>
2023-12-02 15:53:07 +0100

    steps.d.ts aktualisieren

--- steps.d.ts
+++ steps.d.ts
@@ -4,11 +4,10 @@
 type steps_file = typeof import('./steps_file');
 type loginPage = typeof import('./pages/Login');
 type CDPHelper = import('./helpers/CDPHelper');
-type ExpectHelper = import('codeceptjs-expect');
 
 declare namespace CodeceptJS {
   interface SupportObject { I: I, current: any, loginPage: loginPage }
-  interface Methods extends Playwright, CDPHelper, OpenAI, ExpectHelper, REST {}
+  interface Methods extends Playwright, CDPHelper, OpenAI, Expect, REST {}
   interface I extends ReturnType<steps_file>, WithTranslation<Methods> {}
   namespace Translation {
     interface Actions {}
